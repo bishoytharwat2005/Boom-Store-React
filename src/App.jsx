3fb82eb6f7914c11f95a6e398e933f98fe9c2aa7
Bishoy Tharwat <byshwythrwt8@gmail.com>
2026-07-30 15:09:26 +0300

    firstcommit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import PostContent from './components/PostContent'
+
+function App() {
+  return (
+    <div>
+      <PostContent/>
+    </div>
+  )
+}
+
+export default App
